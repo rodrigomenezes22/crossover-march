57f5d23d5348d73b2bad859a34a54be576525939
rodrigomenezes22 <69732792+rodrigomenezes22@users.noreply.github.com>
2023-03-22 10:29:40 +0100

    Search

--- src/Components/AllResults.js
+++ src/Components/AllResults.js
@@ -1,15 +1,14 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
 import { useState } from 'react';
-import { useNavigate, useParams, Link } from "react-router-dom";
+import { useParams } from 'react-router-dom'
 import Unit from "./Unit";
 
-
-function SearchResults() {
+function AllResults() {
 
     const [ results, setResults ] = useState();
-    const { city } = useParams();
-    console.log(city);
+    
+
     const options = {
         method: 'POST',
         url: 'https://realtor.p.rapidapi.com/properties/v3/list',
@@ -18,7 +17,7 @@ function SearchResults() {
           'X-RapidAPI-Key': '60d8a2760bmshb7963e82b124735p173e03jsnab27911f181c',
           'X-RapidAPI-Host': 'realtor.p.rapidapi.com'
         },
-        data: `{"limit":10,"offset":0,"city":"${city}","status":["for_sale","ready_to_build"],"sort":{"direction":"desc","field":"list_date"}}`
+        data: `{"limit":10,"offset":0,"city":"new york","status":["for_sale","ready_to_build"],"sort":{"direction":"desc","field":"list_date"}}`
       };
     //   Fetch Data
       useEffect(()=> {
@@ -35,9 +34,9 @@ function SearchResults() {
 
   return (
     <div>
-      {results && results.map((home) => (<div key={home.listing_id}><Unit image={home.listing_id} name={home.listing_id} price={home.listing_id} address={home.listing_id} state={home.listing_id} country={home.listing_id} beds={home.listing_id} bath={home.listing_id} url={home.listing_id} /></div>))}
+      
     </div>
   )
 }
 
-export default SearchResults
+export default AllResults
