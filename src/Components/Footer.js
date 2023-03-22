b9027a7419f26506b5553fb0257bb0dc2ff6ce43
Sujit <sb2011ams@gmail.com>
2023-03-22 15:00:11 +0100

    added footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import { FaFacebook } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
+import {
+  Box,
+  Container,
+  Row,
+  Column,
+  FooterLink,
+  Heading,
+} from "./FooterStyles";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
-
-export default Footer
+    <Box>
+      <Container>
+        <Row>
+          <Column>
+            <Heading>About Us</Heading>
+            <FooterLink href="#">Vision</FooterLink>
+            <FooterLink href="#">Awards</FooterLink>
+            <FooterLink href="#">Testimonials</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Services</Heading>
+            <FooterLink href="#">Buy</FooterLink>
+            <FooterLink href="#">Sell</FooterLink>
+            <FooterLink href="#">Management</FooterLink>
+            <FooterLink href="#">Renovations</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Top Cities</Heading>
+            <FooterLink href="#">New York</FooterLink>
+            <FooterLink href="#">San Francisco</FooterLink>
+            <FooterLink href="#">Austin</FooterLink>
+            <FooterLink href="#">Portland</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Socials</Heading>
+            <FooterLink href="#">
+              <FaFacebook className="icon" />
+              Facebook
+            </FooterLink>
+            <FooterLink href="#">
+              <FaLinkedin className="icon" />
+              LinkedIn
+            </FooterLink>
+            <FooterLink href="#">
+              <AiFillInstagram className="icon" />
+              Instagram
+            </FooterLink>
+          </Column>
+        </Row>
+      </Container>
+    </Box>
+  );
+};
+export default Footer;
