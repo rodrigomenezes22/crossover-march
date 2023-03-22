04fbfcb17be9833a456658b807e76344379fc142
Sujit <sb2011ams@gmail.com>
2023-03-22 13:06:25 +0100

    some cleanup and header

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,10 @@
+export default function SearchBar() {
+  return (
+    <div>
+      <div className="search-bar">
+        <input type="text" />
+        <button className="btn btn-primary">Search</button>
+      </div>
+    </div>
+  );
+}
